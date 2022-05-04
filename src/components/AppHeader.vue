ff5338a315cc43e6e6f968a4f6fc2247932e8c28
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-05-04 18:57:16 +0200

    Index: aggiunta colore testo active in  header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -90,13 +90,16 @@ export default {
         position: relative;
         display: flex;
         justify-content: center;
-        &:hover::after{
-          position: absolute;
-          bottom: 0;
-          content: '';
-          width: 100%;
-          height: 3px;
-          background-color: $coloractive;
+        &:hover{
+          color: $coloractive;
+          &::after{
+            position: absolute;
+            bottom: 0;
+            content: '';
+            width: 100%;
+            height: 3px;
+            background-color: $coloractive;
+          }
         }
       }
     }
