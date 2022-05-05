9f78da30ca1829e53c5194ee7e178ab6d0b3ddd2
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-05-05 13:49:56 +0200

    Index: completamento footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,12 @@
 <template>
     <footer>
-        <div class="products">
+        <div class="top-footer">
             <div class="list" v-for="(prodotto, index) in products" :key="index">
                 <img :src="prodotto.image" width="30" height="30" :alt="prodotto.nome">
                 <span>{{prodotto.nome}}</span>
             </div>
         </div>
-        <div class="alldc">
+        <div class="mid-footer">
             <div class="utilities">
                 <div>
                     <h2>dc comics</h2>
@@ -35,8 +35,14 @@
             </div>
             <img src="../assets/images/dc-logo-bg.png" width="500" height="500" alt="DC Logo">
         </div>
-        <div>
-
+        <div class="bottom-footer">
+            <button>sign-up now!</button>
+            <div class="follow">
+                <h3>follow us</h3>
+                <div v-for="social in socials" :key="social">
+                    <img :src="social.link" :alt="social.testo">
+                </div>
+            </div>
         </div>
     </footer>
 </template>
@@ -102,7 +108,29 @@ export default {
             shop: [
                 "Shop DC",
                 "Shop DC Collectibles" 
-            ]
+            ],
+            socials:[
+                {
+                testo: "facebook",
+                link: "./images/footer-facebook.png"
+                },
+                {
+                testo: "twitter",
+                link: "./images/footer-twitter.png"
+                },
+                {
+                testo: "youtube",
+                link: "./images/footer-youtube.png"
+                },
+                {
+                testo: "pinterest",
+                link: "./images/footer-pinterest.png"
+                },
+                {
+                testo: "periscope",
+                link: "./images/footer-periscope.png"
+                },
+            ],
         }
     }
 }
@@ -111,40 +139,61 @@ export default {
 <style lang="scss" scoped>
 @import '../style/variables';
 @import '../style/general';
-.products{
+.top-footer{
     height: 120px;
     @include my-flex;
     background-color: $bgproducts;
     color: $colortextmain;
-
-}
-.list{
+    .list{
     @include my-flex;
-    span{
-        padding-left: 10px;
+        span{
+            padding-left: 10px;
+        }
     }
 }
-.alldc{
+.mid-footer{
     @include my-flex;
     height: 350px;
     overflow: hidden;
     background-image: url(../assets/images/footer-bg.jpg);
-}
-.utilities{
+    .utilities{
     width: 30%;
     color: $colortextmain;
     display: flex;
     flex-wrap: wrap;
-    div{
-        padding: 0 5px;
+        div{
+            padding: 0 5px;
+            h2{
+                text-transform: uppercase;
+                font-size: 20px;
+            }
+            li{
+                list-style-type: none;
+                font-size: 15px;
+            }
+        }
     }
 }
-h2{
-    text-transform: uppercase;
-    font-size: 20px;
-}
-li{
-    list-style-type: none;
-    font-size: 15px;
+.bottom-footer{
+    @include my-flex;
+    height: 100px;
+    background-color: $bgfooterbottom;
+    button{
+        border: 2px solid $colortextactive;
+        padding: 10px;
+        background-color: transparent;
+        color: $colortextmain;
+        text-transform: uppercase;
+    }
+    .follow{
+        @include my-flex;
+        h3{
+           color: $colortextactive; 
+           text-transform: uppercase; 
+        }
+        img{
+            padding-left: 20px;
+        }
+    }
 }
 </style>
